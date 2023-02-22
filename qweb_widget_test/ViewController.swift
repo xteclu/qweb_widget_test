f073e6a7ac85177169e311690f8b12fdaa35a77f
nurken <xteclu@gmail.com>
2023-02-22 17:50:18 +0600

    Initial Commit

---
+++ qweb_widget_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qweb_widget_test
+//
+//  Created by Nurken Tileubergenov on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
